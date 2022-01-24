f0b2c5d3bc83c2743a1319cebb4f123ba2a9e524
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-01-24 14:59:30 -0800

    got home tests to pass

--- src/views/Home/Home.test.jsx
+++ src/views/Home/Home.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import App from '../../App'
 
 const user = {
   id: 1,
@@ -11,4 +12,20 @@ const user = {
   color: 'crimson',
 }
 
-test('Should render the user profile', () => {})
+test('Should render the user profile', async () => {
+  render(<App />)
+
+  const name = await screen.findByText(/Vonta/i)
+  const header = await screen.findByAltText(/header/i)
+  const avatar = await screen.findByAltText(/avatar/i)
+  const motto = await screen.findByLabelText(/motto/i)
+  const interests = await screen.findByText(/interests/i)
+  const list = await screen.findAllByRole('listitem')
+
+  expect(name).toBeInTheDocument()
+  expect(header).toBeInTheDocument()
+  expect(avatar).toBeInTheDocument()
+  expect(motto).toBeInTheDocument()
+  expect(interests).toBeInTheDocument()
+  expect(list).toHaveLength(6)
+})
